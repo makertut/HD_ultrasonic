fd519a24ae6c5acad8aa72923d59aaa925aa6fe4
jumejume1 <jumejume1@hotmail.com>
2020-08-05 08:53:22 +0700

    Create ex1.ino

---
+++ ex1.ino
@@ -0,0 +1,21 @@
+float Inch=0.00;
+float cm=0.00;
+int SonarPin=A0;
+int sensorValue;
+
+void setup(){
+    pinMode(SonarPin,INPUT);
+    Serial.begin(9600);
+}
+void loop(){
+    sensorValue=analogRead(SonarPin);
+    delay(50);
+    Inch= (sensorValue*0.497);
+    cm=Inch*2.54;
+    Serial.println(sensorValue);
+    Serial.print(Inch);
+    Serial.println("inch");
+    Serial.print(cm);
+    Serial.println("cm");
+    delay(100);
+}
